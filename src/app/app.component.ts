64949f88ebe5dec4d113a6b37ec25190f25a46bd
LindaSchxfer <lindaschaefer98@gmail.com>
2022-01-31 15:22:42 +0100

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-pwa';
+
+  constructor(swUpdate: SwUpdate){
+  }
 }
